5f53cf1eb782b4aaf613953ba643efcd76a26a04
Ayu0317mi <n0217ayumi@gmail.com>
2025-01-27 17:03:34 +1000

    Modify styles on home-mission.scss

--- src/views/pages/home/mission.js
+++ src/views/pages/home/mission.js
@@ -8,27 +8,37 @@ export default function Mission() {
             <div class="mission-contentsbox">
                 <div class="mission-box">
                     <div class="title">Mission</div>
-                    <div>
-                        <div class="mission-textbox">
-                            <div class="textbox-line"></div>
-                            <div class="textbox-title">
-                                Lorem ipsum dolor sit amet, 
-                                consectetur adipiscing elit, sed
-                                do eiusmod tempor incididunt.
-                            </div>
 
-                            <p class="textbox-text">
-                            Lorem ipsum dolor sit amet, <br>
-                            consectetur adipiscing elit, sed do eiusmod
-                            tempor incididunt ut labore et dolore magna aliqua.
-                            Ut enim ad minim veniam, quis nostrud exercitation <br>
-                            ullamco laboris nisi ut aliquip ex ea commodo consequat.<br>
-                            Duis aute irure dolor in reprehenderit in voluptate <br>
-                            velit esse cillum dolore eu fugiat nulla pariatur.
-                            </p>               
+                    <div class="mission-textbox">
+                        <div class="textbox-line"></div>
+                        <div class="textbox-title">
+                            Lorem ipsum dolor sit amet, 
+                            consectetur adipiscing elit, sed
+                            do eiusmod tempor incididunt.
                         </div>
-                        <div class="mission-button">Find out more</div>
+
+                        <p class="textbox-text">
+                        Welcome to DUO Rowing, where our commitment to quality meets a deep passion for the sport. 
+                        Founded by a former coxswain, DUO embodies the spirit of partnership and teamwork that defines rowing. 
+                        The name "DUO" reflects not only my journey as a coxswain but also the invaluable bonds formed along the way
+                        </p>   
+                        
+                        <p class="textbox-text">
+                        As a coxswain, I learned that the smallest details can make the biggest difference. 
+                        That’s why we specialise in high-quality rowing seat wheels designed for smooth, 
+                        effortless movement. We believe that every rower deserves equipment that performs flawlessly, 
+                        allowing them to focus on their technique and enjoyment of the sport.
+                        </p>  
+
+                        <p class="textbox-text">
+                        At DUO, we are committed to providing durable and functional rowing wheels that meet the needs of both casual 
+                        enthusiasts and competitive athletes. Our products are crafted from premium materials, 
+                        ensuring longevity and reliability in all conditions.
+                        </p>  
+
                     </div>
+
+                    <div class="mission-button">Find out more</div>
                 </div>
             </div>
         </div>    
