185f378f136560a510fd4b164a7202f221fb6e8d
Ayu0317mi <n0217ayumi@gmail.com>
2025-01-27 15:47:18 +1000

    modify font-size for min-width  screens on home-products.scss

--- src/views/pages/home/mission.js
+++ src/views/pages/home/mission.js
@@ -6,31 +6,30 @@ export default function Mission() {
         <div class="mission-container">
             <img src="/images/home_02.webp" alt="Home mission">
             <div class="mission-contentsbox">
-            <div class="mission-box">
-                <div class="title">Mission</div>
-                <div>
-                    <div class="mission-textbox">
-                        <div class="textbox-line"></div>
-                        <div class="textbox-title">
-                            Lorem ipsum dolor sit amet, 
-                            consectetur adipiscing elit, sed
-                            do eiusmod tempor incididunt.
-                        </div>
+                <div class="mission-box">
+                    <div class="title">Mission</div>
+                    <div>
+                        <div class="mission-textbox">
+                            <div class="textbox-line"></div>
+                            <div class="textbox-title">
+                                Lorem ipsum dolor sit amet, 
+                                consectetur adipiscing elit, sed
+                                do eiusmod tempor incididunt.
+                            </div>
 
-                        <p class="textbox-text">
-                        Lorem ipsum dolor sit amet, <br>
-                        consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua.
-                        Ut enim ad minim veniam, quis nostrud exercitation <br>
-                        ullamco laboris nisi ut aliquip ex ea commodo consequat.<br>
-                        Duis aute irure dolor in reprehenderit in voluptate <br>
-                        velit esse cillum dolore eu fugiat nulla pariatur.
-                        </p>               
+                            <p class="textbox-text">
+                            Lorem ipsum dolor sit amet, <br>
+                            consectetur adipiscing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua.
+                            Ut enim ad minim veniam, quis nostrud exercitation <br>
+                            ullamco laboris nisi ut aliquip ex ea commodo consequat.<br>
+                            Duis aute irure dolor in reprehenderit in voluptate <br>
+                            velit esse cillum dolore eu fugiat nulla pariatur.
+                            </p>               
+                        </div>
+                        <div class="mission-button">Find out more</div>
                     </div>
-                    <div class="mission-button">Find out more</div>
                 </div>
-            
-            </div>
             </div>
         </div>    
     `;
